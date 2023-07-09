c36fbaea3db39e2f90ba59b27c41ca18d04d617c
ibnul.imtiaz <imtiazeemel42@gmail.com>
2023-07-09 16:16:11 +0600

    Column

---
+++ app/src/main/java/com/imtiaz/composeui/RowColumnBox.kt
@@ -0,0 +1,38 @@
+package com.imtiaz.composeui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true, widthDp = 300, heightDp = 300)
+@Composable
+fun ColumnBasicLayoutComposable(){
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Text1", fontSize = 24.sp)
+        Text(text = "Text2", fontSize = 24.sp)
+    }
+
+}
+
+
+@Preview(showBackground = true, widthDp = 300, heightDp = 300)
+@Composable
+fun RowBasicLayoutComposable(){
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Text(text = "Text1", fontSize = 24.sp)
+        Text(text = "Text2", fontSize = 24.sp)
+    }
+
+}
